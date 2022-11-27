3d7cd7c03240b15ac41872f709e271c94401acee
Taylor Sweetman <taylor.sweetman@gmail.com>
2022-11-27 10:36:39 -0500

    first command working

--- bin/index.js
+++ bin/index.js
@@ -1,19 +1,30 @@
+#! /usr/bin/env node
+
 import { execSync } from "child_process";
 import { Command } from "commander";
+
+const toStdOut = (buf) => {
+  console.log(buf.toString().slice(0, -1));
+};
 
 const main = () => {
   const program = new Command();
 
   program
-    .name("d-b")
+    .name("db")
     .description("A collection of aliases for running Docker commands");
 
-  program.command("dps").action(() => {
-    try {
-      const dps = execSync("docker ps");
-      console.log(dps.toString());
-    } catch (error) {}
-  });
+  program
+    .command("ps")
+    .option("-a, --all", "Show all containers (default shows just running)")
+    .action((options) => {
+      try {
+        const dps = options.all
+          ? execSync("docker ps -a")
+          : execSync("docker ps");
+        toStdOut(dps);
+      } catch (error) {}
+    });
 
   program.parse();
 };
