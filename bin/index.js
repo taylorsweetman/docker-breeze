532680f5156df33bf2e39f52cfc39ac69bf558cc
Taylor Sweetman <taylor.sweetman@gmail.com>
2022-11-26 20:11:34 -0500

    started using commander

--- bin/index.js
+++ bin/index.js
@@ -1,9 +1,21 @@
 import { execSync } from "child_process";
+import { Command } from "commander";
 
 const main = () => {
-  console.log("Hello world!");
-  const dps = execSync("docker ps");
-  console.log(dps.toString());
+  const program = new Command();
+
+  program
+    .name("d-b")
+    .description("A collection of aliases for running Docker commands");
+
+  program.command("dps").action(() => {
+    try {
+      const dps = execSync("docker ps");
+      console.log(dps.toString());
+    } catch (error) {}
+  });
+
+  program.parse();
 };
 
 main();
